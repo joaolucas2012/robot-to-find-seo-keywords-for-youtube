dfb6a557cf7bb229ef02717e3d15bb2e21b3cfec
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-10-27 18:13:11 -0300

    :boom: [FIX] fix project to get the correct results of keywords and print them in the terminal

--- index.js
+++ index.js
@@ -1,77 +1,85 @@
 const puppeteer = require("puppeteer");
 
 const bot = async () => {
+  // print the greetings
+  print("Welcome to the keywords finder!");
+
   // open the browser window
+  print("Launching in the browser...");
   const browser = await puppeteer.launch({
     headless: false,
     args: ["--start-maximized"],
     defaultViewport: null,
   });
+  print("Successfully launched.");
+  print("Opening a new page...");
   const page = await browser.newPage();
+  print("Successfully opened.");
 
   // open the website to search keywords
   await page.goto("https://rapidtags.io/generator");
+  print("Launched in the website!");
 
   // get the agree button and click on it
   await page.click("#agree");
+  print("Button agree clicked!");
 
   // keyword to search for
-  const keyword = "como cortar vídeos pelo kinemaster";
-  print(`Pesquisando por "${keyword}"`);
+  const keyword = "chocolate";
+  print(`Pesquisando por "${keyword}..."`);
 
   // get the input element and clear it
   const inputElement = await page.$x('//*[@id="searchInput"]');
   inputElement.value = "";
 
+  // type the keyword to search for
   await page.type("#searchInput", keyword);
 
   // get the search button and click on it
   await page.click("#search-wrapper > div.input-wrapper > label > button");
+  print("Button search clicked!");
 
-  console.log("before waiting");
-  await delay(4000);
-  console.log("after waiting");
+  // wait for 2000 miliseconds
+  await delay(2000);
 
   // get the copy button and click on it
+  print("Copying the text...");
   await page.click(".copy");
 
   // reload the page
+  print("Reloading the page...");
   await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+  print("Page successfully reloaded!");
 
-  console.log("before waiting");
-  await delay(4000);
-  console.log("after waiting");
+  // wait for 2000 miliseconds
+  await delay(2000);
 
   // focus in the input element
   await page.focus("#searchInput");
 
   // wait for 500 miliseconds
-  console.log("before waiting");
   await delay(500);
-  console.log("after waiting");
 
   // paste the keyboards in the input element
+  print("Pasting the keywords in the input...");
   await page.keyboard.down("Control");
   await page.keyboard.press("KeyV");
   await page.keyboard.up("Control");
 
-  // wait for 100 miliseconds
-  console.log("before waiting");
+  // wait for 500 miliseconds
   await delay(500);
-  console.log("after waiting");
 
-  // copy the keyboards of the input element
-  const keywords = await page.evaluate(() => {
-    document.querySelector("#searchInput").value;
+  // get the keyboards of the input element
+  print("Getting the result...");
+  const result = await page.evaluate(() => {
+    const input = document.querySelector("#searchInput");
+    return input.value;
   });
 
-  // wait for 500 miliseconds
-  console.log("before waiting");
-  await delay(50);
-  console.log("after waiting");
-  console.log(keywords);
+  // print the keywords in the terminal
+  print(`"\n"These are the resulting keywords: ${result}`);
 
-  print("Encerrando robô...");
+  print("\nEncerrando robô...");
   await browser.close();
 };
 
