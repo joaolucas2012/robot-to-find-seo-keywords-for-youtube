1157dac9867273754ee83c61ede3d70ef06fa7f4
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-10-27 15:57:05 -0300

    :rocket: [ADD] create project

--- index.js
+++ index.js
@@ -2,63 +2,88 @@ const puppeteer = require("puppeteer");
 
 const bot = async () => {
   // open the browser window
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+    defaultViewport: null,
+  });
   const page = await browser.newPage();
 
   // open the website to search keywords
   await page.goto("https://rapidtags.io/generator");
 
+  // get the agree button and click on it
+  await page.click("#agree");
+
   // keyword to search for
   const keyword = "como cortar vídeos pelo kinemaster";
   print(`Pesquisando por "${keyword}"`);
 
-  await page.evaluate(() => {
-    document.querySelector("#searchInput").value = "";
-  });
+  // get the input element and clear it
+  const inputElement = await page.$x('//*[@id="searchInput"]');
+  inputElement.value = "";
 
   await page.type("#searchInput", keyword);
 
-  await page.evaluate(async () => {
-    await document
-      .querySelector("#search-wrapper > div.input-wrapper > label > button")
-      .click();
+  // get the search button and click on it
+  await page.click("#search-wrapper > div.input-wrapper > label > button");
+
+  console.log("before waiting");
+  await delay(4000);
+  console.log("after waiting");
+
+  // get the copy button and click on it
+  await page.click(".copy");
+
+  // reload the page
+  await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+
+  console.log("before waiting");
+  await delay(4000);
+  console.log("after waiting");
+
+  // focus in the input element
+  await page.focus("#searchInput");
+
+  // wait for 500 miliseconds
+  console.log("before waiting");
+  await delay(500);
+  console.log("after waiting");
+
+  // paste the keyboards in the input element
+  await page.keyboard.down("Control");
+  await page.keyboard.press("KeyV");
+  await page.keyboard.up("Control");
+
+  // wait for 100 miliseconds
+  console.log("before waiting");
+  await delay(500);
+  console.log("after waiting");
+
+  // copy the keyboards of the input element
+  const keywords = await page.evaluate(() => {
+    document.querySelector("#searchInput").value;
   });
 
-  await page.evaluate(() => {
-    // click button copy
-    // await document.querySelector(".copy").click();
-
-    const num = document.querySelector(
-      "#tag-generator > div.tagbox"
-    ).childElementCount;
-
-    const keys = [];
-    let element;
-
-    for (let i = 1; i <= num; i++) {
-      element = document.querySelector(
-        `#tag-generator > div.tagbox > span:nth-child(${i})`
-      ).textContent;
-      keys.push(element);
-    }
-    console.log(keys);
-  });
-
-  // open a new page in the browser
-  // const newPage = await browser.newPage();
-
-  // navigate to the google docs page
-  // await newPage.goto(
-  //   "https://docs.google.com/document/d/1X5hEv2g5xOqXs_ixBHY1nU27WjuBZvjvOVIi1wFDseQ/edit?usp=sharing"
-  // );
+  // wait for 500 miliseconds
+  console.log("before waiting");
+  await delay(50);
+  console.log("after waiting");
+  console.log(keywords);
 
   print("Encerrando robô...");
   await browser.close();
 };
 
 // function to print things
-const print = (thing) => {
+function print(thing) {
   console.log(thing);
-};
+}
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
 
 bot();
